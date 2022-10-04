cb3bfb974a7adf79f788af5a1399704b7a653100
Muhammad Adeen <112985225+Adeen317@users.noreply.github.com>
2022-10-05 02:53:36 +0500

    Add files via upload

---
+++ Day13/day13.sv
@@ -0,0 +1,43 @@
+// Various ways to implement a mux
+
+module day13 (
+  input     wire[3:0] a_i,
+  input     wire[3:0] sel_i,
+
+  output    wire     ternary_out,
+  output    logic     case_o,
+  output    logic     ifelse_o,
+  output    logic     loop_o,
+  output    logic     and_or_o
+);
+
+  // Ternary operator
+  assign ternary_out = sel_i[0] ? a_i[0] :sel_i[1] ? a_i[1] :sel_i[2] ? a_i[2] :a_i[3];
+
+  // Using case
+  always_comb begin
+    case (sel_i)
+      4'b0001 : case_o = a_i[0];
+      4'b0010 : case_o = a_i[1];
+      4'b0100 : case_o = a_i[2];
+      4'b1000 : case_o = a_i[3];
+      default : case_o = 1'bx;
+    endcase
+  end
+
+  // Using ifelse
+  always_comb begin
+    if (sel_i[0])      ifelse_o = a_i[0];
+    else if (sel_i[1]) ifelse_o = a_i[1];
+    else if (sel_i[2]) ifelse_o = a_i[2];
+    else if (sel_i[3]) ifelse_o = a_i[3];
+    else               ifelse_o = 1'bx;
+  end
+
+  // Using and-or tree
+  assign and_or_o = (sel_i[0] & a_i[0]) |
+                   (sel_i[1] & a_i[1]) |
+                   (sel_i[2] & a_i[2]) |
+                   (sel_i[3] & a_i[3]);
+
+endmodule
