21780f091f39abfbccdf28e3925d181e078c10ad
Muhammad Adeen <112985225+Adeen317@users.noreply.github.com>
2022-10-25 01:40:41 +0500

    Add files via upload

---
+++ Day22/day22.sv
@@ -0,0 +1,13 @@
+// Hello world from SV classes
+
+class day22;
+
+  function new();
+    // Leave Empty
+  endfunction
+
+  function void hello();
+    $display("MERL, 2022\n");
+  endfunction
+
+endclass
